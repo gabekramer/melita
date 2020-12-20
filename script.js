cb9e4000523b92181f0de0843c15cbc7ff1188ed
gabekramer <gabekramer03@gmail.com>
2020-12-20 17:17:32 +0000

    lvl1 progress

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var state = 0; // walking state for cat
 var lastUpdate = Date.now(); // Time sense last update in ms
 var dy = 0;
 var intro = true;
-var jump = false;
+var grounded = true;
 var gravity = false;
 var map = false;
 var lvl1_var = false;
@@ -16,6 +16,11 @@ var lvl2_var = false;
 var lvl2_var = false;
 var gameover = false;
 var contact = false;
+var damage = false;
+var enemy1_movement_r = false;
+var enemy1_movement_l = false;
+var over_enemy1 = false;
+var over_land1 = false;
 // Add key listener and key states
 var keyMap = {
   68: 'right',
@@ -37,14 +42,20 @@ window.addEventListener("keyup", keyup, false)
 /////////////////
 // Load images //
 /////////////////
+images.flag = document.getElementById('flag');
 images.peppa_R1 = document.getElementById('peppa_R1');
 images.peppa_R2 = document.getElementById('peppa_R2');
 images.peppa_L1 = document.getElementById('peppa_L1');
 images.peppa_L2 = document.getElementById('peppa_L2');
+images.heart = document.getElementById('heart');
+images.dark_heart = document.getElementById('dark_heart');
 images.logo = document.getElementById('logo');
-images.main_ground = document.getElementById('main_ground');
-images.daisy1 = document.getElementById('daisy1');
-images.daisy2 = document.getElementById('daisy2');
+images.gameover = document.getElementById('gameover');
+images.dirt = document.getElementById('main_ground');
+images.daisyR1 = document.getElementById('daisyR1');
+images.daisyR2 = document.getElementById('daisyR2');
+images.daisyL1 = document.getElementById('daisyL1');
+images.daisyL2 = document.getElementById('daisyL2');
 images.lvl1 = document.getElementById('lvl1');
 images.lvl2 = document.getElementById('lvl2');
 images.lvl3 = document.getElementById('lvl3');
@@ -55,23 +66,31 @@ images.lvl3 = document.getElementById('lvl3');
 
 
 // entities
-
+var land1 = new entity(images.dirt, (0), (.65),(1/10), (0) )
+var heart1 = new entity(images.heart, (0), (0), (0), (1/30/canvas.height*canvas.width));
+var heart2 = new entity(images.heart, (.038), (0), (0), (1/30/canvas.height*canvas.width));
 var logo = new entity(images.logo, (5/10)-((3/10)*(1/1.3)/2), (-1.1), (3/10)*(1/1.3), (3/10));
-var ground = new entity(images.main_ground, (0), (4/5),  (1),            (1/5));
-var enemy = new entity(images.peppa_R1, (-1), (7/10), (1/10)*(1/1.3), (0));
-var player = new entity(images.daisy1,      (0), (1), (1/10)*(1/1.3), (1/10));
+var gameover_logo = new entity(images.gameover, (1/2-(1/2)*(1/1.3)/2), (-1/2), (1/2)*(1/1.3), (1/4));
+var ground = new entity(images.dirt, (0), (4/5),  (1),            (1/5));
+var enemy1 = new entity(images.peppa_R1, (-1), (11/15), (1/15)*(1/1.3), (0));
+var player = new entity(images.daisyR1,      (0), (1), (1/10)*(1/1.3), (1/10));
 var lvl1 = new entity(images.lvl1, 1/20, 1/30, (1/15), (0), 1);
 //ad 7/60 to x pos
-var lvl2 = new entity(images.lvl2, (1/6), 1/30, (1/15), (0), 2);
+var lvl2 = new entity(images.lvl2, (1/6), (1/30), (1/15), (0), 2);
 var lvl3 = new entity(images.lvl3, (17/60), 1/30, (1/15), (0), 3);
 // World init (add entities to world, etc)
-world.append(enemy);
+world.append(enemy1);
+world.append(land1);
+world.append(heart1);
+world.append(heart2);
 world.append(lvl1);
 world.append(lvl2);
 world.append(lvl3);
 world.append(logo);
 world.append(ground);
 world.append(player);
+world.append(gameover_logo);
+
 
 
 // Main tick
@@ -85,8 +104,11 @@ updateManager.startTick(50); // specify  the tps
 function lvl1_func(){
 	map = false;
 	lvl1_var = true;
-	enemy.x = (1/3);
-	enemy.h = (1/10);
+	land1.h = 1/30;
+	land1.x = .87;
+	enemy1.x = (1/3);
+	enemy1.h = (1/15);
+	enemy1_movement_r = true;
 }
 
 function lvl2_func(){
@@ -110,7 +132,9 @@ function worldObject() { // holds all the objects/entities in the world
 		this.entities.push(entity);
 	}
 }
-
+function stop_game(){
+	updateManager.stopTick();
+}
 function updateManagerObject(world) { // manages tick updates
 	
 	
@@ -135,7 +159,9 @@ function updateManagerObject(world) { // manages tick updates
 		/////////////////
 		// Update game //
 		/////////////////
-		var player_movement = false;
+	
+	var player_movement_r = false
+	var player_movement_l = false;
 	if(intro == true && keyStates["jump"]==true){
 		player.x = 0;
 		intro = false;
@@ -151,27 +177,93 @@ function updateManagerObject(world) { // manages tick updates
 		lvl2.h = lvl2.w/canvas.height*canvas.width;
 		lvl3.h = lvl3.w/canvas.height*canvas.width;
 	}
-	if (map == false){
+	if (map == false ){
 		player.h = (1/10);
 		ground.w = 1;
 		lvl1.h = 0;
 		lvl2.h = 0;
 		lvl3.h = 0;
 	}
-	if(intro == false && map == false && player.y>(ground.y-player.h)-0.02 && (keyStates["up"]==true || keyStates["jump"]==true)){
-		dy = .6;
-	}	
-	var player_bottom = player.y + player.h;
-	var player_width = player.x + player.w;
-	var enemy_bottom = enemy.y + enemy.h;
-	var enemy_width = enemy.x + enemy.w;
-	
-	if(player_width >= enemy.x && player_width <= enemy_width && player_bottom - .02 <= enemy_bottom && player_bottom >= enemy.y ){
-		contact = true;
-		console.log('die');
+	if (map == false && intro == false){
+		heart1.w = (1/30);
+		heart2.w = (1/30);
 	}
 
+
+	//collison code
+	var player_bottom = player.y + player.h;
+	var player_width = player.x + player.w;
+	var enemy1_bottom = enemy1.y + enemy1.h;
+	var enemy1_width = enemy1.x + enemy1.w;
+	var land1_width = land1.x + land1.w;
 	
+	if((player_width >= enemy1.x && player_width <= enemy1_width) ||(player.x >= enemy1.x && player.x <= enemy1_width)){
+		over_enemy1 = true;
+	}
+	
+	else{
+		over_enemy1 = false;
+	}
+
+	if((player_width >= land1.x && player_width <= land1_width) ||(player.x >= land1.x && player.x <= land1_width)){
+		over_land1 = true;
+	}
+	else{
+		over_land1 = false;
+	}
+
+	if(over_land1 == true && player_bottom >= land1.y && player_bottom <= land1.y + .1 && dy < 0){
+		grounded = true;
+	}
+	
+
+	if(over_enemy1 == true && player_bottom >= enemy1.y && player_bottom >= enemy1.y - .1 && dy < 0 ){
+		enemy1.y = 1;
+		enemy1.h = 0;
+		dy = .3;
+	}
+
+
+	if(over_enemy1 == true && player_bottom -.02 <= enemy1_bottom && player_bottom >= enemy1.y ){
+		contact = true;
+	}
+	else{
+		contact = false;
+	}
+
+	if(contact == true && heart2.img == images.heart){
+		heart2.img = images.dark_heart;
+	}
+	if(contact == false && heart2.img == images.dark_heart){
+		damage = true;
+	}
+	if(contact == true && damage == true){
+		heart1.img = images.dark_heart;
+		gameover = true;
+	}
+	if(gameover == true && gameover_logo.y <= (11/20)){
+		player.h= 0;
+		enemy1.h = 0;
+		gameover_logo.y+=timePassed/1000*0.4;
+	}
+	if(gameover_logo.y >= (11/20)){
+		gameover_logo.y = (11/20);
+		updateManager.stopTick()
+	}
+
+	if(gameover_logo.y == (11/20) && (keyStates["jump"]==true)){
+		console.log('test');
+		updateManager.startTick();
+		gameover_logo.y = -1/2;
+		intro = true;
+		damage = false;
+		gameover = false;
+		player.x = 0;
+		player.h = 1/10;
+		logo.y = -1.1;
+
+	}
+
 	if (logo.y <= (4.9/10)){
 		logo.y += .4 * (timePassed/1000);
 	}
@@ -179,43 +271,115 @@ function updateManagerObject(world) { // manages tick updates
 		logo.y = (4.9/10);	
 	}
 	if(logo.y == (4.9/10) && intro == true && player.x <= 1){
-		player_movement = true;
+		player_movement_r = true;
 		player.x += (timePassed/1000) * .4;	
 	};
 	
 	// Update y player_movement 
-	
-	if (dy != 0) {
-		player.y -= dy * (timePassed/1000); // Move the player vert
+	if(gravity == true){
 		dy -= 1 * (timePassed/1000); // Gravity
 	}
 	
-	if (player.y>(ground.y-player.h)) { // Make sure the player can't fall into the ground
-		dy = 0; // Stop y player_movement
-		player.y = ground.y-player.h; // Set player to ground
+	if(grounded == true){
+		dy = 0;
+		gravity = false;
+	}
+	
+	if (dy != 0) {
+		player.y -= dy * (timePassed/1000); // Move the player vert
+		gravity = true;
 	}
 
-		if (intro == false && map == false && keyStates["left"]==true && player.x>0) {
+	
+	if (player.y>(ground.y-player.h)) { // Make sure the player can't fall into the ground
+		grounded = true; // Stop y player_movement
+		player.y = ground.y-player.h; // Set player to ground
+	}
+		//lvl 1 functions
+		
+		if(lvl1_var == true && enemy1.x <= (1/3) ){
+			enemy1_movement_r = true;
+			enemy1_movement_l = false;
+		}
+
+		if(lvl1_var == true && enemy1.x >= (1/2) ){
+			enemy1_movement_l = true;
+			enemy1_movement_r = false;
+			}
+
+		if(lvl1_var == true  && enemy1_movement_l == true){
+			enemy1.x -= (timePassed/1000*0.15)
+		}
+
+		if(lvl1_var == true  && enemy1_movement_r == true){
+			enemy1.x += (timePassed/1000*0.15)
+		}
+
+		if(gameover == false && intro == false && map == false && grounded == true && (keyStates["up"]==true || keyStates["jump"]==true)){
+			grounded = false;
+			dy = .6;
+	}	
+
+		if (gameover == false && intro == false && map == false && keyStates["left"]==true && player.x>0) {
 			
-			player_movement = true;
+			player_movement_l = true;
 			player.x-= (timePassed/1000) * 0.3;
 		}
-		if (intro == false && map == false && keyStates["right"]==true && player.x+player.w<1) {
+		if (gameover == false && intro == false && map == false && keyStates["right"]==true && player.x+player.w<1) {
 			
-			player_movement = true;
+			player_movement_r = true;
 			player.x+=timePassed/1000*0.3;		}
 		// Cat images state
-		if (player_movement) {
+		if (player_movement_r) {
 			if (state < 1) {
-				player.img = images.daisy1;
+				player.img = images.daisyR1;
 				state += timePassed/1000*3;
 			} else if (state < 2) {
-				player.img = images.daisy2;
+				player.img = images.daisyR2;
 				state += timePassed/1000*3;
 			} else {
 				state = 0;
 			}
 		}
+
+
+		if (player_movement_l) {
+			if (state < 1) {
+				player.img = images.daisyL1;
+				state += timePassed/1000*3;
+			} else if (state < 2) {
+				player.img = images.daisyL2;
+				state += timePassed/1000*3;
+			} else {
+				state = 0;
+			}
+		}
+
+
+		if (enemy1_movement_r) {
+			if (state < 1) {
+				enemy1.img = images.peppa_R1
+				state += timePassed/1000*3;
+			} else if (state < 2) {
+				enemy1.img = images.peppa_R2;
+				state += timePassed/1000*3;
+			} else {
+				state = 0;
+			}
+		}
+
+		if (enemy1_movement_l) {
+			if (state < 1) {
+				enemy1.img = images.peppa_L1;
+				state += timePassed/1000*3;
+			} else if (state < 2) {
+				enemy1.img = images.peppa_L2;
+				state += timePassed/1000*3;
+			} else {
+				state = 0;
+			}
+		}
+
 		/////////////////////
 		// Update graphics //
 		/////////////////////
