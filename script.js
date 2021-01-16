93733ca5e8ba5bbbb75b4f634c2cdf592a1338c3
gabekramer <gabekramer03@gmail.com>
2021-01-16 18:26:15 +0000

    mouse pos

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext("2d");
 
+// http://jsfiddle.net/sierawski/4xezb7nL/
+//example code for mouse pos
+
 var world = new worldObject();
 var updateManager = new updateManagerObject(world);
 var images = [];
 var state = 0; // walking state for cat
 var lastUpdate = Date.now(); // Time sense last update in ms
 var dy = 0;
+var console_timer = 0;
+
 var intro = true;
 var grounded = true;
 var gravity = false;
@@ -19,32 +24,42 @@ var contact = false;
 var damage = false;
 var enemy1_movement_r = false;
 var enemy1_movement_l = false;
-var jump = false;
+var jump_var = false;
 var over_enemy1 = false;
-var land_contact = false;
+var object_contact = false;
+var over_lvl1 = false;
+var over_lvl2 = false;
+var over_lvl3 = false;
 var over_land1 = false;
 var over_land2 = false;
 var over_land3 = false;
 var over_land4 = false;
 var reeses1_collected = false;
 var all_collected = false;
+var mouse_x;
+var mouse_y;
 // Add key listener and key states
 var keyMap = {
-  68: 'right',
-  65: 'left',
-  87: 'up',
-  83: 'down',
-	32: 'jump'
+  68: 'd',
+  65: 'a',
+  87: 'w',
+	32: 'space',
+	37: 'left',
+	38: 'up',
+	39: 'right'
+	
 }
 var keyStates = {
-		left: false,
-    right: false,
-    up: false,
+		a: false,
+    d: false,
+    w: false,
     down: false,
-		jump: false
+		space: false
 }
-window.addEventListener("keydown", keydown, false)
-window.addEventListener("keyup", keyup, false)
+//event listeners
+window.addEventListener("keydown", keydown, false);
+window.addEventListener("keyup", keyup, false);
+window.addEventListener('mousemove', update_mouse, false);
 
 /////////////////
 // Load images //
@@ -65,9 +80,12 @@ images.daisyR1 = document.getElementById('daisyR1');
 images.daisyR2 = document.getElementById('daisyR2');
 images.daisyL1 = document.getElementById('daisyL1');
 images.daisyL2 = document.getElementById('daisyL2');
-images.lvl1 = document.getElementById('lvl1');
-images.lvl2 = document.getElementById('lvl2');
-images.lvl3 = document.getElementById('lvl3');
+images.lvl1_unlocked = document.getElementById('lvl1_unlocked');
+images.lvl2_unlocked = document.getElementById('lvl2_unlocked');
+images.lvl3_unlocked = document.getElementById('lvl3_unlocked');
+images.lvl1_locked = document.getElementById('lvl1_locked');
+images.lvl2_locked = document.getElementById('lvl2_locked');
+images.lvl3_locked = document.getElementById('lvl3_locked');
 ///////////////////
 // Load entities //
 ///////////////////
@@ -75,6 +93,7 @@ images.lvl3 = document.getElementById('lvl3');
 
 
 // entities
+
 var reeses1 = new entity(images.reeses, (0), (.51), (1/20)*(1/1.3), (0));
 var flag = new entity(images.flag1, (0), (.31), (1/15)*(1/1.3), (0) );
 var land1 = new entity(images.dirt, (0), (.65),(1/10), (0) );
@@ -88,10 +107,14 @@ var gameover_logo = new entity(images.gameover, (1/2-(1/2)*(1/1.3)/2), (-1/2), (
 var ground = new entity(images.dirt, (0), (4/5),  (1),            (1/5));
 var enemy1 = new entity(images.peppa_R1, (-1), (11/15), (1/15)*(1/1.3), (0));
 var player = new entity(images.daisyR1,      (0), (1), (1/10)*(1/1.3), (1/10));
-var lvl1 = new entity(images.lvl1, 1/20, 1/30, (1/15), (0), 1);
+var lvl1 = new entity(images.lvl1_unlocked, 1/20, 1/30, (1/15), (0), 1);
 //ad 7/60 to x pos
-var lvl2 = new entity(images.lvl2, (1/6), (1/30), (1/15), (0), 2);
-var lvl3 = new entity(images.lvl3, (17/60), 1/30, (1/15), (0), 3);
+var lvl2 = new entity(images.lvl2_locked, (1/6), (1/30), (1/15), (0), 2);
+var lvl3 = new entity(images.lvl3_locked, (17/60), 1/30, (1/15), (0), 3);
+
+// ZZ test
+  
+
 // World init (add entities to world, etc)
 world.append(enemy1);
 world.append(flag);
@@ -119,41 +142,68 @@ updateManager.startTick(50); // specify  the tps
 /////////////////
 /// Functions ///
 /////////////////
+function update_mouse(event){
+	rect = canvas.getBoundingClientRect();
+
+  mouse_x = (1*event.x-rect.left)/(rect.width); 
+	mouse_y = (1*event.y-rect.top)/(rect.height);
+  
+}
+canvas.addEventListener('click', lvl1_func)
+
+
 
 function lvl1_func(){
-	map = false;
-	lvl1_var = true;
-	land1.h = 1/30;
-	land2.h = 1/30;
-	land3.h = 1/30;
-	land4.h = 1/30;
-	land1.x = .87;
-	land2.x = .55;
-  land3.x = .87;
-  land4.x = .18;
-	flag.h = 1/15;
-	flag.x = .894;
-	reeses1.h = 1/25;
-	reeses1.x = .21;
-	enemy1.x = (1/3);
-	enemy1.h = (1/15);
-	enemy1_movement_r = true;
+	if(intro == false && over_lvl1 == true){
+		map = false;
+		lvl1_var = true;
+		land1.h = 1/30;
+		land2.h = 1/30;
+		land3.h = 1/30;
+		land4.h = 1/30;
+		land1.x = .87;
+		land2.x = .55;
+  	land3.x = .87;
+  	land4.x = .18;
+		land1.y = .65;
+		land2.y = .55;
+		land3.y = .38;
+		land4.y = .55;
+		flag.h = 1/15;
+		flag.x = .894;
+		reeses1.h = 1/25;
+		reeses1.x = .21;
+		enemy1.x = (1/3);
+		enemy1.h = (1/15);
+	}	enemy1_movement_r = true;
 }
 
 function lvl2_func(){
-	map = false;
-	lvl2_var = true;
+	/*if(intro == false){
+		map = false;
+		lvl2_var = true;
+		land1.x = .18;
+		land1.y = .65;
+		land2.y = .6;
+		land4.y = .05;
+
+		
+	}
+	*/
 }
 function lvl3_func(){
 	map = false;
 	lvl2_var = true;
 }
+function update_x(){
+	//console.log(pos.x/canvas.width);
 
+}
 
 //fuck fuck fuck ive tried so much shit
-canvas.addEventListener('click', lvl1_func);
-canvas.addEventListener('click', lvl2_func);
-canvas.addEventListener('click', lvl3_func);
+//document.lvl1.addEventListener('click', lvl1_func);
+//canvas..addEventListener('click', lvl2_func);
+//canvas.lvl3.addEventListener('click', lvl3_func);
 
 function worldObject() { // holds all the objects/entities in the world
 	this.entities = [];
@@ -188,10 +238,18 @@ function updateManagerObject(world) { // manages tick updates
 		/////////////////
 		// Update game //
 		/////////////////
-	
+		
+		console_timer += (timePassed/1000) * 1;
+		
+		if(console_timer >= .5){
+			console_timer = 0;
+			//console.log(mouse_x);
+		}
+
+
 	var player_movement_r = false
 	var player_movement_l = false;
-	if(intro == true && keyStates["jump"]==true){
+	if(intro == true && keyStates["space"]==true){
 		player.x = 0;
 		intro = false;
 		logo.w = 0;
@@ -202,6 +260,15 @@ function updateManagerObject(world) { // manages tick updates
 	if (map == true){
 		player.h = 0;
 		ground.w = 0;
+		heart1.w = 0;
+		heart2.w = 0;
+		land1.h = 0;
+		land2.h = 0;
+		land3.h = 0;
+		land4.h = 0;
+		reeses1.h = 0;
+		enemy1.h = 0;
+		flag.h = 0;
 		lvl1.h = lvl1.w/canvas.height*canvas.width;
 		lvl2.h = lvl2.w/canvas.height*canvas.width;
 		lvl3.h = lvl3.w/canvas.height*canvas.width;
@@ -218,9 +285,21 @@ function updateManagerObject(world) { // manages tick updates
 		heart2.w = (1/30);
 	}
 
+	document.getElementById('lvl1_unlocked').width = lvl1.w;
+	document.getElementById('lvl1_unlocked').height = lvl1.h;
 
 	//collison code
 	
+	if(mouse_x >= lvl1.x && mouse_x <= lvl1.width && mouse_y >= lvl1.y && mouse_y <= lvl1.bottom){
+		over_lvl1 = true;
+	}
+	else{
+		over_lvl1 = false}
+	
+		
+	
+
+
 	if(player.bottom <= reeses1.bottom && player.bottom >= reeses1.bottom - .001 && player.width >= reeses1.x && player.x <= reeses1.width){
 		reeses1.h = 0;
 		reeses1_collected = true;
@@ -232,8 +311,10 @@ function updateManagerObject(world) { // manages tick updates
 		flag.img = images.flag2;
 	}
 
-	if(flag.img == images.flag2 && player.bottom <= flag.bottom && flag.bottom >= flag.bottom - .001 && player.width >= flag.x && player.x <= flag.width){
-		alert('congrats you won but I still need to add the stuff that happens after u win');
+	if(lvl1_var == true && flag.img == images.flag2 && player.bottom <= flag.bottom && player.bottom >= flag.bottom - .01 && player.bottom >= flag.y && player.width >= flag.x && player.x <= flag.width){
+		lvl2.img = images.lvl2_unlocked;
+		map = true;
+		lvl1_var = false;
 	}
 
 	if((player.width >= enemy1.x && player.width <= enemy1.width) ||(player.x >= enemy1.x && player.x <= enemy1.width)){
@@ -250,7 +331,7 @@ function updateManagerObject(world) { // manages tick updates
 		dy = .3;
 	}
 	//land1
-	if(player.width >= land1.x + .01 && player.width <= land1.width ||player.x >= land1.x  && player.x <= land1.width){
+	if(player.width >= land1.x + .005 && player.width <= land1.width ||player.x >= land1.x  && player.x <= land1.width){
     over_land1 = true;
     over_land3 = true;
   }
@@ -277,7 +358,7 @@ function updateManagerObject(world) { // manages tick updates
 
 //land 2
 	
-	if(player.width >= land2.x + .01 && player.width <= land2.width ||player.x >= land2.x && player.x <= land2.width - .01){
+	if(player.width >= land2.x + .005 && player.width <= land2.width ||player.x >= land2.x && player.x <= land2.width - .005){
     over_land2 = true;
   }
  
@@ -302,6 +383,16 @@ function updateManagerObject(world) { // manages tick updates
   }
 	//land3
 
+	if(player.width >= land3.x + .005 && player.width <= land1.width ||player.x >= land1.x  && player.x <= land3.width){
+    over_land3 = true;
+    
+  }
+ 
+  else{
+  
+    over_land3 = false;
+  }
+
 
 	if(over_land3 == true && player.bottom >= land3.y && player.bottom <= land3.y + .1 && dy < 0){
 		player.y = land3.y - player.h;
@@ -321,7 +412,7 @@ function updateManagerObject(world) { // manages tick updates
 	//land 4
 
 
-	if(player.width >= land4.x + .01 && player.width <= land4.width ||player.x >= land4.x && player.x <= land4.width - .01){
+	if(player.width >= land4.x + .005 && player.width <= land4.width ||player.x >= land4.x && player.x <= land4.width - .005){
     over_land4 = true;
   }
  
@@ -336,7 +427,7 @@ function updateManagerObject(world) { // manages tick updates
 	}
 	
 
-	if(over_land4 == false && player.bottom >= land4.y && player.bottom <= land4.y + .1 && player.x >= land4.width && player.x <= land4.width + player.w + .01 || over_land4 == false && player.bottom >= land4.y && player.bottom <= land4.y + .1 && player.width <= land4.y && player.width <= land4.y + player.w + .01){
+	if(over_land4 == false && player.bottom >= land4.y && player.bottom <= land4.y + .1 && player.x >= land4.width && player.x <= land4.width + player.w + .1 || over_land4 == false && player.bottom >= land4.y && player.bottom <= land4.y + .1 && player.width <= land4.y && player.width <= land4.y + player.w + .01){
     gravity = true;
     grounded = false;
   }
@@ -348,35 +439,37 @@ function updateManagerObject(world) { // manages tick updates
 
 //the .1 is to add the gap when player is moviang and the .01 keeps the player x from changing when hitting side of land
 	if(player.width >= land1.x && player.width <= land1.x + .01 && player.bottom <= land1.bottom + player.h && player.bottom >= land1.y +.01){
-		land_contact = true;
+		object_contact = true;
     grounded = false;
 		
 		
 	}
 	else if(player.bottom <= land2.bottom + player.h && player.bottom >= land2.y +.01 && player.width >= land2.x && player.width <= land2.x + .1|| player.bottom <= land2.bottom + player.h && player.bottom >= land2.y +.01 && player.x <= land2.width && player.x >= land2.width - .01){
-		land_contact = true;
+		object_contact = true;
     grounded = false;	
   }
 
 	else if(player.bottom <= land3.bottom + player.h && player.bottom >= land3.y +.01 && player.width >= land3.x && player.width <= land3.x + .1|| player.bottom <= land3.bottom + player.h && player.bottom >= land3.y +.01 && player.x <= land3.width && player.x >= land3.width - .01){
-		land_contact = true;
+		object_contact = true;
     grounded = false;	
   }
 
 
 	else if(player.bottom <= land4.bottom + player.h && player.bottom >= land4.y +.01 && player.width >= land4.x && player.width <= land4.x + .1|| player.bottom <= land4.bottom + player.h && player.bottom >= land4.y +.01 && player.x <= land4.width && player.x >= land4.width - .01){
-		land_contact = true;
+		object_contact = true;
     grounded = false;	
   }
 
 	else{
-		land_contact = false;
+		object_contact = false;
 	}
 
 	if(over_enemy1 == true && player.bottom -.02 <= enemy1.bottom && player.bottom >= enemy1.y ){
+		object_contact = true;
 		contact = true;
 	}
 	else{
+		object_contact = false;
 		contact = false;
 	}
 
@@ -391,17 +484,26 @@ function updateManagerObject(world) { // manages tick updates
 		gameover = true;
 	}
 	if(gameover == true && gameover_logo.y <= (11/20)){
-		player.h= 0;
+		player.h = 0;
+		heart1.w = 0;
+		heart2.w = 0;
+		land1.h = 0;
+		land2.h = 0;
+		land3.h = 0;
+		land4.h = 0;
+		reeses1.h = 0;
 		enemy1.h = 0;
+		flag.h = 0;
 		gameover_logo.y+=timePassed/1000*0.4;
+
 	}
 	if(gameover_logo.y >= (11/20)){
 		gameover_logo.y = (11/20);
 		updateManager.stopTick()
 	}
 
-	if(gameover_logo.y == (11/20) && (keyStates["jump"]==true)){
-		console.log('test');
+	if(gameover_logo.y == (11/20) && (keyStates["space"]==true)){
+		//console.log('test');
 		updateManager.startTick();
 		gameover_logo.y = -1/2;
 		intro = true;
@@ -466,21 +568,21 @@ function updateManagerObject(world) { // manages tick updates
 			enemy1.x += (timePassed/1000*0.15)
 		}
 
-		if(gameover == false && intro == false && map == false && grounded == true && (keyStates["up"]==true || keyStates["jump"]==true)){
-			jump = true;
+		if(gameover == false && intro == false && map == false && grounded == true && (keyStates["w"]==true || keyStates["space"]==true || keyStates["up"]==true)){
+			jump_var = true;
 			grounded = false;
 			dy = .6;
 		}
 		else{
-			jump = false;
+			jump_var = false;
 		}	
 
-		if (gameover == false && intro == false && map == false && land_contact == false && keyStates["left"]==true && player.x>0) {
+		if (gameover == false && intro == false && map == false && object_contact == false && (keyStates["a"]==true || keyStates["left"]==true) && player.x>0) {
 			
 			player_movement_l = true;
 			player.x-= (timePassed/1000) * 0.3;
 		}
-		if (gameover == false && intro == false && map == false && land_contact == false && keyStates["right"]==true && player.width<1) {
+		if (gameover == false && intro == false && map == false && object_contact == false && (keyStates["d"]==true || keyStates["right"]==true) && player.width<1) {
 			
 			player_movement_r = true;
 			player.x+=timePassed/1000*0.3;	
